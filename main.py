9d11178b3233b964eb50d6828a4c241955a490da
camille0814 <72763933+camille0814@users.noreply.github.com>
2021-01-19 10:17:48 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+while True:
+    music.set_volume(60)
+    music.play_melody("E B C5 A G F", 120)
---
+++ main.ts
@@ -0,0 +1,4 @@
+while (true) {
+    music.setVolume(60)
+    music.playMelody("E B C5 A G F", 120)
+}
